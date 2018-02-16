c0946e5001e9d5df33a5b0c7049cbe3e42057453
Jonas Braun <jbraun@physik.hu-berlin.de>
2018-02-16 14:13:04 +0100

    fix imports, allow arbitrary community names

--- matching.py
+++ matching.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
 
+import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 
 def match(timeseries, memory=2, *,
@@ -29,9 +32,9 @@ def match(timeseries, memory=2, *,
     memory -- the number of timesteps to look back in the timeseries
     for matching.
 
-    memory_weights -- a list of length (memory) with weights. if None is
-    given, 1/i weighting is used so older communities have less influence
-    on the matching score.
+    memory_weights -- memory kernel; a list of length (memory) with
+    weights. if None is given, 1/i weighting is used so older communities
+    have less influence on the matching score.
 
     score_threshold -- memory weighted jaccard indices under the
     threshold will not be included in the matching
@@ -65,10 +68,11 @@ def match(timeseries, memory=2, *,
         for j in range(1, memory+1):
 
             # compare with timestep i-j
-            #print('memory', j, i-j)
             
             if i-j < 0:
                 break         # beginning of timeseries reached
+
+            #print('memory', j, i-j)
 
             communities = timeseries[i-j]
 
@@ -77,12 +81,12 @@ def match(timeseries, memory=2, *,
                                     len(communities)), dtype=np.float)
         
 
-            for k, A in base_communities.items():
+            for k, (b_name, A) in enumerate(base_communities.items()):
         
             
-                for l, B in communities.items():
+                for l, (name, B) in enumerate(communities.items()):
                     
-                    if (i-j,l) in seen:
+                    if (i-j,name) in seen:
                         # this community is part of an already detected
                         # temporal community. its jaccard index (* memory
                         # weight) was already added to the score of the 
@@ -102,7 +106,7 @@ def match(timeseries, memory=2, *,
                         scores for past members of the temp. comm. to
                         this communities' score (within memory range)
                         """
-                        timestep, group = i-j, l
+                        timestep, group = i-j, name
                         
                         while True:
                             try:
@@ -140,23 +144,21 @@ def match(timeseries, memory=2, *,
             
             all_match_costs.append(match_costs)
             timesteps.append(i-j)
-            
+
+
         # aggregate results from memory steps
         #print(all_match_costs)
         match_costs_array = np.hstack(all_match_costs)
-        sizes = [_[1] for _ in map(np.shape, all_match_costs)]
-        group_names_dict = { }  # keeps track of indices in stacked array
-        counter = 0
-        for e, s in enumerate(sizes):
-            group_names_dict.update({_+counter:(timesteps[e],_) for _ in range(s)})
-            counter += s
-            
+        base_community_names = list(timeseries[i].keys())
+        community_names = []
+        for t in timesteps:
+            community_names.extend([ ( t, _) for _ in timeseries[t].keys() ])
 
         # match
         matches = np.dstack(linear_sum_assignment(match_costs_array))[0]
 
 
-        # filter (only matches above threshold
+        # filter (only matches above threshold)
         for k, l in matches:
 
             #print(j, k)
@@ -164,13 +166,64 @@ def match(timeseries, memory=2, *,
             if match_costs_array[k][l] > - score_threshold:
                 continue
             
-            #print('match')
-            #print(match_costs[j][k])
-        
-            temporal_communities_dict[(i,k)] = group_names_dict[l]
+
+            temporal_communities_dict[(i,base_community_names[k])] \
+                    = community_names[l]
         
 
     return temporal_communities_dict
 
 
 
+def aggregate_temporal_communities(temporal_communities_dict):
+    """
+    from a chain of recognized links between communities, follow the
+    chains to find all groups of communities belonging to one temp. comm.
+    """
+
+    temporal_communities = defaultdict(set)
+    seen = {}   # helper dict
+
+    for k, v in temporal_communities_dict.items():
+        
+        #print(k,v)
+        
+        chain = [k,v]
+
+        # find first node in chain
+        while True:
+            # follow chain
+            try:
+                v = temporal_communities_dict[v]
+            except KeyError:
+                break
+                
+            # check if part of chain was already traversed
+            try:
+                v = seen[v]
+                break
+            except KeyError:
+                pass
+                
+            chain.append(v)
+            
+        #print(chain)
+            
+        # append; final v is chain name   
+        temporal_communities[v] |= set(chain)
+            
+        seen[k] = v   # save shortcut to first of chain
+        
+    return temporal_communities
+
+
+def matching(*args, **kwargs):
+    """
+    high level function for matching and formating of results
+    """
+
+    temporal_communities_dict = match(*args, **kwargs)
+
+    temporal_communities = aggregate_temporal_communities(temporal_communities_dict)
+
+    return list(temporal_communities.values())
